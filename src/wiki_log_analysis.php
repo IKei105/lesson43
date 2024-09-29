6f80edf33ca818ee826568ceb81569cf28676219
IKei105 <natsuki.kei105@gmail.com>
2024-09-30 00:34:31 +0900

    コードを見やすくしました

--- src/wiki_log_analysis.php
+++ src/wiki_log_analysis.php
@@ -1,62 +1,22 @@
 <?php
 
-$limit = 1;
-
-$mysqli = new mysqli('db', 'root', 'pass', 'wiki_log');
-if ($mysqli->connect_error) {
-    echo '接続失敗'.PHP_EOL;
-    exit();
-} else {
-    echo '接続成功'.PHP_EOL;
-}
-
+$mysqli = connectMySQL();
 $input = userInput();
-
-if (is_numeric($input[0])) {
-    ///これは数字だからlimitに入れることにな
-    $sql = <<<EOT
-    SELECT language, page_name, request_times
-    FROM wiki_log_2021_1201_12
-    LIMIT ?;
-EOT;
-    $stmt = $mysqli->prepare($sql);
-    $limitNum = (int)$input[0];
-    $stmt->bind_param('i', $limitNum);
-    if ($stmt->execute()) {
-        $result = $stmt->get_result();
-        while ($row = mysqli_fetch_assoc($result)) {
-            //echo "Page Name: " . $row['page_name'] . ", Language: " . $row['language'] . PHP_EOL;
-            echo '"' . $row['language'] . '","Main_page,"' .  $row['page_name'] . PHP_EOL;
-        }
-    } else {
-        echo "エラー: " . $stmt->error;
-    }
-} else {
-    //これは文字列だからselectに使うことになる
-    //配列の数だけSQLを実行する
-    $sql = <<<EOT
-    SELECT language, page_name, request_times
-    FROM wiki_log_2021_1201_12
-    WHERE language = ?
-    ORDER BY language
-    LIMIT 1;
-EOT;
-    foreach($input as $domain) {
-        $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param('s', $domain);
-        if ($stmt->execute()) {
-            $result = $stmt->get_result();
-            while ($row = mysqli_fetch_assoc($result)) {
-                //echo "Page Name: " . $row['page_name'] . ", Language: " . $row['language'] . PHP_EOL;
-                echo '"' . $row['language'] . '"' .  $row['request_times'] . PHP_EOL;
-            }
-        } else {
-            echo "エラー: " . $stmt->error;
-        }   
-    }
-}
-
+displayResult($input, $mysqli);
 mysqli_close($mysqli);
+
+function connectMySQL()
+{
+    $mysqli = new mysqli('db', 'root', 'pass', 'wiki_log');
+    if ($mysqli->connect_error) {
+        echo '接続失敗'.PHP_EOL;
+        exit();
+    } else {
+        echo '接続成功'.PHP_EOL;
+    }
+    
+    return $mysqli;
+}
 
 function userInput(): array
 {
@@ -66,8 +26,36 @@ function userInput(): array
     return $input;
 }
 
+function displayResult($input, $mysqli)
+{
+    $limit = 1;
+    if (is_numeric($input[0])) {
+        $sql = 'SELECT language, page_name, request_times FROM wiki_log_2021_1201_12 LIMIT ?';
+        $stmt = $mysqli->prepare($sql);
+        $limitNum = (int)$input[0];
+        $stmt->bind_param('i', $limitNum);
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '"' . $row['language'] . '","Main_page,"' .  $row['page_name'] . PHP_EOL;
+            }
+        } else {
+            echo "エラー: " . $stmt->error;
+        }
+    } else {
+        $sql = 'SELECT language, page_name, request_times FROM wiki_log_2021_1201_12 WHERE language = ? ORDER BY language LIMIT 1';
+        foreach($input as $domain) {
+            $stmt = $mysqli->prepare($sql);
+            $stmt->bind_param('s', $domain);
+            if ($stmt->execute()) {
+                $result = $stmt->get_result();
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo '"' . $row['language'] . '"' .  $row['request_times'] . PHP_EOL;
+                }
+            } else {
+                echo "エラー: " . $stmt->error;
+            }   
+        }
+    }
+}
 
-
-
-
-?>
